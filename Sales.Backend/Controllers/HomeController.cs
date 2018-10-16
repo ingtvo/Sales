258c5e55f6dc3a7e6e48d829386c3d4c4893b82b
Ingtvo <ingtvo@msn.com>
2018-10-16 12:03:03 -0500

    Parte 03

---
+++ Sales.Backend/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+/*
+using System.Data.Client;
+using System.Data;
+using System.Configuration;
+
+    SqlConnection Conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString)
+     */
+
+namespace Sales.Backend.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
